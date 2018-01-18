5252fe3cf8cb5de296dad5876755b95b49d0e478
Dylan Herman <dah242@scarletmail.rutgers.edu>
2018-01-18 08:44:37 -0500

    eval: fixed bugs in eval

--- eval.py
+++ eval.py
@@ -6,6 +6,8 @@ from classification import helpers
 import collections
 
 
+Helper = helpers.Helper
+
 FLAGS = tf.app.flags.FLAGS
 tf.app.flags.DEFINE_string('pipeline_config',None,'')
 
@@ -13,26 +15,28 @@ tf.app.flags.DEFINE_string('pipeline_config',None,'')
 if not FLAGS.pipeline_config:
     raise ValueError("Must specify pipeline config file")
 
-configs = helpers.get_configs_from_pipeline_file(FLAGS.pipeline_config)
+configs = Helper.get_configs_from_pipeline_file(FLAGS.pipeline_config)
 
+model_config = configs["model_config"]
 eval_config = configs["eval_config"]
 eval_input_config = configs["eval_input_config"]
 
 eval_ops_dict = collections.OrderedDict({})
 scalar_updates = []
-with tf.name_scope("eval"):
-    classification_model = model_builder.build(model_config, False)
-    batcher = helpers.get_inputs(eval_input_config, classification_model.preprocess)
-    batched_tensors = batcher.dequeue()
-    predictions = classification_model.predict(batched_tensors["input"])
-    train_loss, train_scalar_updates = helpers.get_loss(predictions, batched_tensors)
-    train_acc_dict = helpers.get_acc(predictions, batched_tensors)
-    scalar_updates += train_scalar_updates
-    eval_ops_dict['loss %.2f '] = train_loss
-    for label in train_acc_dict:
-        eval_ops_dict['eval_'+label+"_acc %.2f "] = train_acc_dict[label]
 
-evaluation_coordinator.EvaluationCoordinator(eval_input_config.\
+classification_model = model_builder.build(model_config, False)
+batcher = Helper.get_inputs(eval_input_config, classification_model.preprocess)
+batched_tensors = batcher.dequeue()
+predictions = classification_model.predict(batched_tensors["input"])
+with tf.name_scope("eval"):
+    train_loss, train_scalar_updates = Helper.get_loss(predictions, batched_tensors)
+    train_acc_dict = Helper.get_acc(predictions, batched_tensors)
+scalar_updates += train_scalar_updates
+eval_ops_dict['loss %.2f '] = train_loss
+for label in train_acc_dict:
+    eval_ops_dict['eval_'+label+"_acc %.2f "] = train_acc_dict[label]
+
+evaluation_coordinator.EvaluationCoordinator(eval_ops_dict,
+                                             eval_input_config.\
                                                 eval_batch_mode).\
-        evaluate(eval_config,
-                eval_ops_dict)
+        evaluate(eval_config)
