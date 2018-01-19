963154b795778ccff793d94a8606b743f32a0dde
Dylan Herman <dah242@scarletmail.rutgers.edu>
2018-01-19 11:01:42 -0500

    inference: started inference

--- inference.py
+++ inference.py
@@ -1,11 +1,12 @@
 import tensorflow as tf
-
+import numpy as np
+from PIL import Image
 """Inference from Frozen Graph from Tensorflow Object Detection API
 """
 
 FLAGS = tf.app.flags.FLAGS
-tf.app.flags.DEFINE_string('model_ckpt',None,'')
 tf.app.flags.DEFINE_string('image_path',None,'')
+tf.app.flags.DEFINE_string('frozen_graph',None,'') # use a frozen pb
 
 classification_graph = tf.Graph()
 with classification_graph.as_default()
@@ -19,17 +20,3 @@ with detection_graph.as_default():
   with tf.Session(graph=detection_graph) as sess:
     # Definite input and output Tensors for detection_graph
     image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
-
-
-with tf.Session() as sess:
-	ckpt = tf.train.get_checkpoint_state(FLAGS.model_ckpt)
-	if ckpt and ckpt.model_checkpoint_path:
-		saver = tf.train.import_meta_graph(ckpt.model_checkpoint_path+'.meta')
-		saver.restore(sess,ckpt.model_checkpoint_path)
-	#logits_train = {'alphanumeric':tf.get_default_graph().get_tensor_by_name('alphanumeric_logits/alphanumeric_logits/BiasAdd:0')}
-	logits = list(map(tf.nn.softmax,[logits_train[l] for l in logits_train.keys()]))
-	img = np.array(Image.open(FLAGS.image_path),dtype='float32')
-	results = [res for res in sess.run(logits,feed_dict={'input_image:0':[img]})]
-	for r in results[0]:
-		print('%.2f'%r)
-	print('Softmax: ',[np.argmax(res) for res in results])
