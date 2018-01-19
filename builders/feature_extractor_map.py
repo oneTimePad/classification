fd8135a68714c25097fd6628828743f167661381
Dylan Herman <dah242@scarletmail.rutgers.edu>
2018-01-19 18:32:59 -0500

    builders: fixed import for mobilenets

--- builders/feature_extractor_map.py
+++ builders/feature_extractor_map.py
@@ -1,9 +1,9 @@
 
 import classification.extractors.inception_v3_feature_extractor as inception_v3_feature_extractor
-
+import classification.extractors.mobile_net_feature_extractor as mobile_net_feature_extractor
 
 NAME_TO_FEATURE_EXTRACTOR = {
     'inception_v3' : inception_v3_feature_extractor.
                         InceptionV3FeatureExtractor,
-    'mobile_nets' : mobile_net_feature_extractor.MobileNetFeatureExtractor 
+    'mobile_nets' : mobile_net_feature_extractor.MobileNetFeatureExtractor
 }
