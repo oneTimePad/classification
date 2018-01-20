8fe38ea0176eaeb069f553b869b2d5325dcc2e47
Dylan Herman <dah242@scarletmail.rutgers.edu>
2018-01-20 12:36:30 -0500

    train: added starts_from

--- trainer.py
+++ trainer.py
@@ -28,7 +28,10 @@ eval_input_config = configs["eval_input_config"]
 
 eval_ops_dict = collections.OrderedDict({})
 scalar_updates = []
-label_names = [label.name for label in model_config.multi_task_label ]
+label_names = [label.name
+                    for label in model_config.multi_task_label ]
+starts_from = {label.name: label.starts_from
+                    for label in model_config.multi_task_label}
 """ Training Model """
 with tf.name_scope("train") as scope:
     is_training = tf.placeholder_with_default(False,shape=(),name="is_training")
@@ -39,8 +42,8 @@ batched_tensors = {label: tensor for label,tensor in batched_tensors.items() if 
 tf.summary.image("train",batched_tensors["input"])
 predictions = classification_model.predict(batched_tensors["input"])
 with tf.name_scope(scope):
-    train_loss, train_scalar_updates = Helper.get_loss(predictions, batched_tensors)
-    train_acc_dict = Helper.get_acc(predictions, batched_tensors)
+    train_loss, train_scalar_updates = Helper.get_loss(predictions, batched_tensors, starts_from)
+    train_acc_dict = Helper.get_acc(predictions, batched_tensors, starts_from)
 scalar_updates += train_scalar_updates
 eval_ops_dict['loss %.2f '] = train_loss
 for label in train_acc_dict:
@@ -59,8 +62,8 @@ if train_config.eval_while_training:
     tf.summary.image("test",batched_tensors["input"])
     predictions = classification_model_test.predict(batched_tensors["input"])
     with tf.name_scope("test"):
-        test_loss, test_scalar_updates = Helper.get_loss(predictions, batched_tensors)
-        test_acc_dict = Helper.get_acc(predictions, batched_tensors)
+        test_loss, test_scalar_updates = Helper.get_loss(predictions, batched_tensors, starts_from)
+        test_acc_dict = Helper.get_acc(predictions, batched_tensors, starts_from)
     scalar_updates += test_scalar_updates
     for label in train_acc_dict:
         eval_ops_dict['test_'+label+"_acc %.2f "] = test_acc_dict[label]
