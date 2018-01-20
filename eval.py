1159cda7eac3240f27e4cc31d7feae9835f11409
Dylan Herman <dah242@scarletmail.rutgers.edu>
2018-01-20 12:36:15 -0500

    eval: added starts_from

--- eval.py
+++ eval.py
@@ -23,15 +23,18 @@ eval_input_config = configs["eval_input_config"]
 
 eval_ops_dict = collections.OrderedDict({})
 scalar_updates = []
-label_names = [label.name for label in model_config.multi_task_label ]
+label_names = [label.name
+                    for label in model_config.multi_task_label ]
+starts_from = {label.name: label.starts_from
+                    for label in model_config.multi_task_label}
 classification_model = model_builder.build(model_config, False)
 batcher = Helper.get_inputs(eval_input_config, classification_model.preprocess)
-batched_tensors = batcher.dequeue()
+batched_tensors = batcher#batcher.dequeue()
 batched_tensors = {label: tensor for label,tensor in batched_tensors.items() if label in label_names or label == "input"}
 predictions = classification_model.predict(batched_tensors["input"])
 with tf.name_scope("eval"):
-    train_loss, train_scalar_updates = Helper.get_loss(predictions, batched_tensors)
-    train_acc_dict = Helper.get_acc(predictions, batched_tensors)
+    train_loss, train_scalar_updates = Helper.get_loss(predictions, batched_tensors, starts_from)
+    train_acc_dict = Helper.get_acc(predictions, batched_tensors, starts_from)
 scalar_updates += train_scalar_updates
 eval_ops_dict['loss %.2f '] = train_loss
 for label in train_acc_dict:
