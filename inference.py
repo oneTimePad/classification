388b02256b17fab04ea892c44abc01f4ddf129c6
Dylan Herman <dah242@scarletmail.rutgers.edu>
2018-01-22 12:35:30 -0500

    infernece: finished

--- inference.py
+++ inference.py
@@ -9,15 +9,14 @@ Helper = helpers.Helper
 """
 
 FLAGS = tf.app.flags.FLAGS
-tf.app.flags.DEFINE_string('pipeline_config',None,'')
+
 tf.app.flags.DEFINE_string('image_path',None,'')
 tf.app.flags.DEFINE_string('frozen_graph',None,'') # use a frozen pb
+tf.app.flags.DEFINE_string('outputs',None,'')
 
-if not FLAGS.pipeline_config:
-    raise ValueError("Must specify pipeline config file")
+if not FLAGS.outputs:
+    raise ValueError("Must specify common separated class outputs")
 
-configs = Helper.get_configs_from_pipeline_file(FLAGS.pipeline_config)
-model_config = configs["model_config"]
 
 classification_graph = tf.Graph()
 with classification_graph.as_default():
@@ -29,8 +28,12 @@ with classification_graph.as_default():
 
 with classification_graph.as_default():
   with tf.Session(graph=classification_graph) as sess:
+    image = np.array(Image.open(FLAGS.image_path),dtype='float32')
+    image = image*2/255.0 -1
     # Definite input and output Tensors for detection_graph
     image_tensor = classification_graph.get_tensor_by_name('image_tensor:0')
-    predictions = {name: classification_graph.get_tensor_by_name("Predictions/"+name+":0") }
-    predictions  = {n.name.split('/')[1]: classification_graph.get_tensor_by_name(n.name + ":0") for n in classification_graph.as_graph_def().node if n.name.startswith("Predictions/")}
+    #predictions = {name: classification_graph.get_tensor_by_name("Predictions/"+name+":0")  for }
+    predictions  = {n:classification_graph.get_tensor_by_name("Predictions/"+n+ ":0") for n in FLAGS.outputs.split(",")}#{n.name.split('/')[1]: classification_graph.get_tensor_by_name(n.name + ":0") for n in classification_graph.as_graph_def().node if n.name.startswith("Predictions/")}
+    softmax = dict(map((lambda kv: (kv[0],tf.nn.softmax(kv[1]))), predictions.items()))
+    predictions = dict(map((lambda kv: (kv[0],np.argmax(kv[1]))),sess.run(softmax,feed_dict={image_tensor:[image]}).items()))
     print(predictions)
