47fce1c84e19418938048ed9b9a734196b32cdc8
Dylan Herman <dah242@scarletmail.rutgers.edu>
2018-02-03 23:19:43 -0500

    Add NASNet

--- builders/feature_extractor_map.py
+++ builders/feature_extractor_map.py
@@ -3,6 +3,7 @@ import classification.extractors.inception_v3_feature_extractor as inception_v3_
 import classification.extractors.mobile_net_feature_extractor as mobile_net_feature_extractor
 import classification.extractors.inception_v4_feature_extractor as inception_v4_feature_extractor
 import classification.extractors.resnet_v2_50_feature_extractor as resnet_v2_50_feature_extractor
+import classification.extractors.nasnet_mobile_feature_extractor as nasnet_mobile_feature_extractor
 
 NAME_TO_FEATURE_EXTRACTOR = {
     'inception_v3': inception_v3_feature_extractor.
@@ -11,5 +12,7 @@ NAME_TO_FEATURE_EXTRACTOR = {
     'inception_v4': inception_v4_feature_extractor.
                         InceptionV4FeatureExtractor,
     'resnet_v2_50'   : resnet_v2_50_feature_extractor.
-                        ResNetV250FeatureExtractor
+                        ResNetV250FeatureExtractor,
+    'nasnet_mobile' : nasnet_mobile_feature_extractor.
+                        NASNetMobileFeatureExtractor
 }
